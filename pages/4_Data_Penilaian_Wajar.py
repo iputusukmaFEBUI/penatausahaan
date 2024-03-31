b0a7d28a24f8cddb0011eb63efdd7f719fc8e509
iputusukmaFEBUI <159942989+iputusukmaFEBUI@users.noreply.github.com>
2024-03-31 22:38:51 +0700

    Add files via upload

---
+++ pages/4_Data_Penilaian_Wajar.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+from streamlit_dynamic_filters import DynamicFilters
+
+st.set_page_config(
+    layout="wide"
+)
+
+st.title("Data Penilaian Wajar Aset")
+
+#st.sidebar.success("pilih tab")
+
+url = "https://docs.google.com/spreadsheets/d/1br1v4cUjKQ7KCFfHaQfWIix-vw4gtm5-wsoRDAZ9dKQ/edit?usp=sharing"
+
+
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+#st.write(st.secrets['connections'])
+data = conn.read(spreadsheet=url,worksheet="1958777433")
+
+dynamic_filters = DynamicFilters(df=data, filters=['Nama Aset'])
+#with st.sidebar:
+dynamic_filters.display_filters()
+dynamic_filters.display_df()
+#st.dataframe(data)
