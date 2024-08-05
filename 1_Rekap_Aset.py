3b6fc80d3d3352f3e730500ae9092ef9dcf054d6
iputusukmaFEBUI <159942989+iputusukmaFEBUI@users.noreply.github.com>
2024-08-05 13:07:55 +0700

    Update 1_Rekap_Aset.py

--- 1_Rekap_Aset.py
+++ 1_Rekap_Aset.py
@@ -17,6 +17,31 @@ conn = st.connection("gsheets", type=GSheetsConnection)
 #st.write(st.secrets['connections'])
 data = conn.read(spreadsheet=url, worksheet="1468705168")
 
+def validate():
+    if '1' not in st.session_state.multiselect:
+        st.session_state.multiselect = ['FY 202']
+
+reportingperiod = st.multiselect(
+    "Select the time to maturity:",
+    options = ("FY 2016","FY 2017","FY 2018",'FY 2019',"FY 2020","FY 2021","FY 2022","FY 2023","SMT I 2024"),
+    default="1",
+    on_change=validate,
+    key='multiselect'
+)
+
+
+data_selection = data.query(
+    "ReportingPeriod == @reportingperiod"
+)
+
+
+
+plotly_figure = px.area(data_frame = data_selection,
+x = df_selection['Periode Laporan'], y=data_selection['Total Nilai'], color = data_selection['ReportingPeriod'], template='seaborn', line_shape='spline',
+title= 'Yield of INDOGB, Maturity in Month(s)', color_discrete_sequence=px.colors.qualitative.Pastel
+)
+
+
 #dynamic_filters = DynamicFilters(df=data, filters=['Nama Aset'])
 #with st.sidebar:
 #dynamic_filters.display_filters()
