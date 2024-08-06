a8c3bbdd3bd2605158a9014a1be2736320e9de5b
iputusukmaFEBUI <159942989+iputusukmaFEBUI@users.noreply.github.com>
2024-08-06 22:50:33 +0700

    Update AnalitikPNBP.py

--- pages/AnalitikPNBP.py
+++ pages/AnalitikPNBP.py
@@ -1,11 +1,8 @@
 import streamlit as st
 import pandas as pd
-import streamlit_pandas as sp
-import os
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
-import plotly.graph_objects as go
 from streamlit_gsheets import GSheetsConnection
 from annotated_text import annotated_text
 
