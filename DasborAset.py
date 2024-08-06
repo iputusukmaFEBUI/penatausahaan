dd77a56192e57cc1281de8a04557661cff1e720e
iputusukmaFEBUI <159942989+iputusukmaFEBUI@users.noreply.github.com>
2024-08-06 22:30:36 +0700

    Update DasborAset.py

--- DasborAset.py
+++ DasborAset.py
@@ -128,7 +128,7 @@ fig_top_spenders_bar.update_layout(
 
 col = st.columns((4, 4, 4), gap='small')
 with col[0]:
-    st.plotly_chart(fig_top_values_bar, theme="streamlit", use_container_width=True)
+    st.plotly_chart(fig_plot, theme="streamlit", use_container_width=True)
 with col[1]:
     st.plotly_chart(fig_top_performers_bar)
 with col[2]:
