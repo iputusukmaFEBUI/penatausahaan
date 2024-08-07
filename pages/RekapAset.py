7eefcc8cdfcf2e2a8a9372395407dc8c5b50dbf0
iputusukmaFEBUI <159942989+iputusukmaFEBUI@users.noreply.github.com>
2024-08-07 17:32:59 +0700

    Update RekapAset.py

--- pages/RekapAset.py
+++ pages/RekapAset.py
@@ -13,19 +13,34 @@ st.set_page_config(
 st.sidebar.header("Penatausahaan Aset LMAN")
 st.sidebar.write("Divisi Anggaran dan Akuntansi @2024")
 
-
 # MAIN PAGE
 st.header(":bar_chart: Daftar Lengkap Aset Kelolaan LMAN ")
 st.markdown("Posisi per 30 Juni 2024")
 st.markdown("---")
 
-
 url = "https://docs.google.com/spreadsheets/d/1br1v4cUjKQ7KCFfHaQfWIix-vw4gtm5-wsoRDAZ9dKQ/edit?usp=sharing"
-
 
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 df = conn.read(spreadsheet=url, worksheet="1480371802")
+
+# TOP METRICS
+nilai_aset= int(df["Nilai Aset"].sum())
+ak_penyusutan = int(df["Ak. Penyusutan"].sum())
+nilai_bersih = int(df_spend["Spend"].sum())
+
+col = st.columns((4, 4, 4), gap='medium')
+with col[0]:
+    st.markdown('**:chart_with_upwards_trend: Nilai Aset**')
+    st.subheader(f"Rp{nilai_aset:,}")
+with col[1]:
+    st.markdown('**:clipboard: Total PNBP**')
+    st.subheader(f"Rp{ak_penyusutan:,}")
+with col[2]:
+    st.markdown('**:pushpinn: Total Spending**')
+    st.subheader(f"Rp{nilai_bersih:,}")
+
+st.markdown("---")
 
 st.dataframe(df)
 
