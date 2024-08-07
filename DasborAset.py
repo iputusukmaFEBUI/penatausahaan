38a7d0d57c461f1bc80c6cfefa8159e6a0b2a27b
iputusukmaFEBUI <159942989+iputusukmaFEBUI@users.noreply.github.com>
2024-08-07 10:16:28 +0700

    Update DasborAset.py

--- DasborAset.py
+++ DasborAset.py
@@ -34,7 +34,7 @@ df_top_spenders_view = df_top_spenders.loc[:,['Nama Aset','Spend']]
 
 # MAIN PAGE
 st.header(":bar_chart: Dasbor Penatausahaan Aset Kelolaan")
-st.markdown("Nilai gross portfolio merupakan nilai aset per 30 Juni 2024 sebelum akumulasi penyusutan. Total PNBP dan total spending merupakan akumulasi sejak tahun 2016 s.d. 30 Juni 2024.")
+st.markdown("Dasbor ini menyajikan informasi penatausahaan aset kelolaan LMAN. yang diadministrasikan oleh unit akuntansi dan pelaporan. Penatausahaan aset kelolaan LMAN dilaksanakan dengan mengacu pada PMK Nomor 144/PMK.06/2020 dan Peraturan Direktur Utama LMAN Nomor 6/LMAN/2021. Nilai aset dan akumulasi penyusutan merupakan posisi per 30 Juni 2024 sebelum akumulasi penyusutan. Total PNBP dan total spending merupakan akumulasi sejak tahun 2016 s.d. 30 Juni 2024. Gross portfolio menunjukkan posisi kelolaan LMAN sebelum akumulasi penyusutan.")
 st.markdown("##")
 
 # TOP METRICS
