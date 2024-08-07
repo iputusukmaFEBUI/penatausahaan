37fa7344560a55efa9110d3b56ddaf052695ffd1
iputusukmaFEBUI <159942989+iputusukmaFEBUI@users.noreply.github.com>
2024-08-07 10:18:44 +0700

    Update DasborAset.py

--- DasborAset.py
+++ DasborAset.py
@@ -11,9 +11,9 @@ st.set_page_config(
     page_icon="📑",
     layout="wide",
     initial_sidebar_state="collapsed"
-    st.sidebar.title("Penatausahaan Aset LMAN")
 )
 
+st.sidebar.title("Penatausahaan Aset LMAN")
 
 #load data
 
