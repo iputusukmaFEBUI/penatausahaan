eef11764a6a04d6022b617ddee16d7e2262750fd
iputusukmaFEBUI <159942989+iputusukmaFEBUI@users.noreply.github.com>
2024-08-07 10:22:38 +0700

    Update RinciAsetProperti.py

--- pages/RinciAsetProperti.py
+++ pages/RinciAsetProperti.py
@@ -12,6 +12,9 @@ st.set_page_config(
     initial_sidebar_state="collapsed"
 )
 
+st.sidebar.header("Penatausahaan Aset LMAN")
+st.sidebar.write("Divisi Anggaran dan Akuntansi @2024")
+
 # MAIN PAGE
 st.header(":bar_chart: Rincian Aset Properti Non-Kawasan")
 st.markdown("##")
