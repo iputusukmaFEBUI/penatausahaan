689eb093b2d10fb8560e909ffd371849fe9ed7ca
iputusukmaFEBUI <159942989+iputusukmaFEBUI@users.noreply.github.com>
2024-08-20 07:35:55 +0700

    Update AnalitikPNBP.py

--- pages/AnalitikPNBP.py
+++ pages/AnalitikPNBP.py
@@ -52,7 +52,7 @@ with middle_column:
     annotated_text((loc_top_average.iloc[0]['Nama Aset'],"","#faa"))
 with right_column:
     st.markdown(":heavy_check_mark: Gross Income Multiplier Tertinggi")
-    st.subheader(f"{top_gim:,.4f} %%")
+    st.subheader(f"{top_gim:,.4f} %")
     #st.write(top_gim)
     annotated_text((loc_top_gim.iloc[0]['Nama Aset'],"","#faa"))
 
